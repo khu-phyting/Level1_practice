c69c3ed4315c38ae8fbde129bbddb3f2ec548294
noah-kwak <56294672+noah-kwak@users.noreply.github.com>
2019-10-15 17:29:13 +0900

    Update 31.문자열을 제거한 뒤 숫자만 반환하기.py

--- 31.문자열을 제거한 뒤 숫자만 반환하기.py
+++ 31.문자열을 제거한 뒤 숫자만 반환하기.py
@@ -1,49 +1,7 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 14,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "키보드를 막 두들기시오: dfpdpsfaopdjfsadㄹ리23905ㅑㅅ4ㅣ;ㄴㅇfdsfjgkf fpwpw레제제제ㅔㅈ 노아 바보\n",
-      "239054\n"
-     ]
-    }
-   ],
-   "source": [
-    "a=input(\"키보드를 막 두들기시오: \")\n",
-    "b=['1','2','3','4','5','6','7','8','9','0']\n",
-    "result=''\n",
-    "for c in a:\n",
-    "    if c in b:\n",
-    "        result=result+c\n",
-    "print(result)"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+a=input("키보드를 막 두들기시오: ")
+b=['1','2','3','4','5','6','7','8','9','0']
+result=''
+for c in a:
+    if c in b:
+        result=result+c
+print(result)
