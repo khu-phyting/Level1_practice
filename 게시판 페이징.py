a4440e14d02d2be0c8ad22c2fa0e875a40461cfd
hongshi97 <56019094+hongshi97@users.noreply.github.com>
2019-10-15 16:49:34 +0900

    Add files via upload

---
+++ 게시판 페이징.py
@@ -0,0 +1,17 @@
+m = int(input("> 총 건수를 입력하시오: "))
+n = int(input(">한 페이지에 보여줄 게시물 수를 입력하시오: "))
+
+if n <= 0:
+    print(("1이상의 수를 입력하시오."))
+    n = int(input(">한 페이지에 보여줄 게시물 수를 입력하시오: "))
+
+if m > n:
+    output = (m // n) + 1
+
+elif m == n:
+    output = (m // n)
+
+else:
+    ouput = (m // n) + 1
+
+print(m, n, output)
