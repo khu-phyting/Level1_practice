1da58e2d1a4bd4919938e1006ea0b13088390fa3
noah-kwak <56294672+noah-kwak@users.noreply.github.com>
2019-10-15 17:28:47 +0900

    Update 28.자신을 제외한 곱셈.py

--- 28.자신을 제외한 곱셈.py
+++ 28.자신을 제외한 곱셈.py
@@ -1,59 +1,14 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "3\n",
-      "6\n",
-      "4\n",
-      "1\n",
-      "(24, 12, 18, 72)\n"
-     ]
-    }
-   ],
-   "source": [
-    "x = []\n",
-    "a = int(input())\n",
-    "b = int(input())\n",
-    "c = int(input())\n",
-    "d = int(input())\n",
-    "\n",
-    "e = b*c*d\n",
-    "f = a*c*d\n",
-    "g = a*b*d\n",
-    "h = a*b*c\n",
-    "\n",
-    "x = (e,f,g,h)\n",
-    "\n",
-    "print(x)\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+x = []
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+e = b*c*d
+f = a*c*d
+g = a*b*d
+h = a*b*c
+
+x = (e,f,g,h)
+
+print(x)
